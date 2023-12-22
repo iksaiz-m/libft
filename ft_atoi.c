7bce1d9870d3fa506455890732580cd04f4ee7c9
Iker Saiz Martinez <iksaiz-m@c3r4s2.42urduliz.com>
2023-12-22 13:21:50 +0100

    Atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: iksaiz-m <iksaiz-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 15:27:16 by iksaiz-m          #+#    #+#             */
-/*   Updated: 2023/12/21 15:28:52 by iksaiz-m         ###   ########.fr       */
+/*   Updated: 2023/12/21 17:54:57 by iksaiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,42 @@
 
 int	ft_atoi(const char *str)
 {
+	int	i;
+	int k;
+	int b;
 	
+	i = 0;
+	k = 0;
+	b = 0;
+	while ((str[i] == '\f' || str[i] == '\n' || str[i] == '\r' 
+		|| str[i] == '\t' || str[i] == '\v' || str[i] == ' '))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			b = 1;
+		i++;
+	}
+	while ((ft_isdigit(str[i])))
+	{
+		k = k * 10 +(str[i] - '0'); 
+		i++;
+	}
+	if (b == 1)
+		return (k * - 1);
+	return (k);
 }
+/*int main()
+{
+	int i;
+	char *s;
+	int a;
+ 
+	s = "a444a";
+	i = atoi(s);
+	a = ft_atoi(s);
+	printf("Original: %d", i);
+	printf("ft: %d", a);
+	return (0);
+}*/
+//((str[i] <= '9' && str[i] >= '0') || (str[i] == '-') || (str[i] == '+'))
