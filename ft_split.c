88638276565fcf2327371c01f2d3f5ba5de2cc74
Iker Saiz Martinez <iksaiz-m@c3r3s2.42urduliz.com>
2024-01-08 18:23:21 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,23 +6,104 @@
 /*   By: iksaiz-m <iksaiz-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 17:58:17 by iksaiz-m          #+#    #+#             */
-/*   Updated: 2024/01/03 18:26:28 by iksaiz-m         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:21:45 by iksaiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Alocar memoria para palabras:
+static char *ft_strndup(const char *s1, size_t n)
+{
+	char *b;
+
+	b = ft_calloc((n + 1), sizeof(char));
+	if (!b)
+		return (NULL);
+	ft_strlcpy(b, s1, n + 1);
+	b[n] = '\0';
+	return (b);
+}
+// Escribe palabras:
+static char *ft_get_word(char *s, char c, size_t *i)
+{
+	char *result;
+	size_t len;
+
+	len = 0;
+	while (s[*i + len] != '\0' && s[*i + len] != c)
+		len++;
+	result = ft_strndup(&s[*i], len);
+	*i += len;
+	while (s[*i] != '\0' && s[*i] == c)
+		*i += 1;
+	return (result);
+}
+
+// Contar palabras
+static size_t ft_num_word(char const *s, char c)
+{
+	size_t num_word;
+	size_t i;
+
+	i = 0;
+	num_word = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			num_word++;
+		i++;
+	}
+	return (num_word);
+}
+
+// Resetear memorias:
+static void ft_free(char **a)
+{
+	size_t t;
+
+	t = 0;
+	while (a[t] != NULL)
+	{
+		free(a[t]);
+		t++;
+	}
+	free(a);
+}
+
 char **ft_split(char const *s, char c)
 {
-    int     a;
-    size_t  i;
+	size_t i;
+	size_t t;
+	char **a;
 
-    i = 0;
-    a = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-            a++;
-        i++;
-    }
+	if (s == NULL)
+		return (NULL);
+	a = (char **)ft_calloc((ft_num_word(s, c) + 1), sizeof(char *));
+	if (a == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0' && s[i] == c)
+		i += 1;
+	t = 0;
+	while (s[i] != '\0')
+	{
+		a[t] = ft_get_word((char *)s, c, &i);
+		if (!a[t])
+			return (ft_free(a), NULL);
+		t++;
+	}
+	return (a);
 }
+
+/* int	main(void)
+{
+	char	**result;
+
+	result = ft_split("p9919 2928928292", ' ');
+	for(int x = 0; result[x] != NULL; x++)
+		printf("%s\n", result[x]);
+	ft_free(result);
+	return (0);
+}
+ */
